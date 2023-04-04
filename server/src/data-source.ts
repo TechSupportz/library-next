eeec5ae9b412ea14b1eb91c3a9eeecce02600b4b
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-04-04 11:17:56 +0800

    migrated to using postgres

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,13 +1,13 @@
 import { DataSource } from "typeorm"
 import { Book } from "./entity/Book"
 
-export const AppDataSource = new DataSource({
-	type: "mysql",
+export const AppDataSource= new DataSource({
+	type: "postgres",
 	host: "localhost",
-	port: 3306,
-	username: "root",
+	port: 5432,
+	username: "postgres",
 	password: "admin",
-	database: "LibraryApp",
+	database: "libraryapp",
 	entities: [Book],
 	synchronize: true,
 	logging: false,
